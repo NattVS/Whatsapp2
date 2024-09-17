426baa2c88c68b8bb4e97ea8a603b7f816dfa2ca
JuanJDlp <juanjosedelapava1@gmail.com>
2024-09-17 18:41:17 -0500

    fix: stoped tracking the target folder

--- server/src/main/java/com/computacion1/ClientHandler.java
+++ server/src/main/java/com/computacion1/ClientHandler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.UUID;
 
