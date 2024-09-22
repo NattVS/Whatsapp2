d21c691df1d0420309bc59acca4d78c73a774481
JuanJDlp <juanjosedelapava1@gmail.com>
2024-09-22 10:11:00 -0500

    feat: Created the esqueleton to implement voice messages

---
+++ server/src/main/java/com/computacion1/AudioFileHandler.java
@@ -0,0 +1,18 @@
+package com.computacion1;
+
+import models.ChatRoom;
+
+public class AudioFileHandler {
+
+    public AudioFileHandler() {
+
+    }
+
+    public void HandleVoiceMessage(ClientHandler user, byte[] payload) {
+
+    }
+
+    public void HandleGroupVoiceMessage(ChatRoom room, byte[] payload){
+
+    }
+}
---
+++ server/src/main/java/models/headers/HeaderServerMessage.java
@@ -0,0 +1,5 @@
+package models.headers;
+
+public class HeaderServerMessage {
+    
+}
